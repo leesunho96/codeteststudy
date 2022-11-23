6af429b579450d87dc6516847ebe3be8df1b468c
leesunho96 <71704395+leesunho96@users.noreply.github.com>
2022-11-23 11:24:36 +0900

    Update 백준1049_기타줄.cpp

--- 백준1049_기타줄.cpp
+++ 백준1049_기타줄.cpp
@@ -20,4 +20,41 @@ int main()
 		cin >> temp.PackagePrice >> temp.SinglePrice;
 		Cases.emplace_back(temp);
 	}
+
+
+	int MinPackagePrice = INT32_MAX, MinSinglePirce = INT32_MAX;
+
+
+	for (auto a : Cases)
+	{
+		if(MinPackagePrice > a.PackagePrice)
+			MinPackagePrice = a.PackagePrice;
+
+		if(MinSinglePirce > a.SinglePrice)
+			MinSinglePirce = a.SinglePrice;
+	}
+	vector<int> prices;
+	int packNum = BrokenString % 6 == 0 ? BrokenString / 6 : (BrokenString / 6) + 1;	
+
+	prices.push_back(BrokenString * MinSinglePirce);
+	prices.push_back(packNum * MinPackagePrice);
+
+	/*int allSinglePrice = BrokenString * MinSinglePirce;
+	int allPackagePrice = packNum * MinPackagePrice;*/
+
+	
+	if (BrokenString % 6 != 0)
+	{
+		prices.push_back(MinPackagePrice * (packNum - 1) + MinSinglePirce * (BrokenString - 6 * (packNum  - 1)));
+	}
+
+
+	int result = INT32_MAX;
+	for (int a : prices)
+	{
+		if(result > a)
+			result = a;
+	}
+	
+	cout << result;	
 }
